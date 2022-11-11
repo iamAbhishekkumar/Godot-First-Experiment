c0aa62cf867c1feb39bd3d620d3e53cffe527293
iamAbhishekkumar <abhisonuk@gmail.com>
2022-11-11 22:48:28 +0530

    intial commit

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+class_name Actor
+
+# for characters, everthing that require collion
+export var speed = Vector2(800,1200)
+export var gravity: = 4000.0
+var _velocity: = Vector2.ZERO
+const FLOOR_NORMAL = Vector2.UP
+func _physics_process(delta: float) -> void:
+	pass
+	
+	
+
